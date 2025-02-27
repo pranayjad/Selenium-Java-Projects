ffefb1bae5f09856f7055b34b99290b3b8e5a2ad
pranay_jadhav <pranayjad@gmail.com>
2025-02-27 20:50:28 +0530

    Selenium TestNG Cucumber POM framework template

---
+++ Selenium_TestNG_Cucumber_Template/src/test/java/template/cucumber/TestNGTestRunner.java
@@ -0,0 +1,9 @@
+package template.cucumber;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features = "src/test/java/template/cucumber",glue = "practice.stepDefinition",
+        monochrome = true,tags="@Regression" ,plugin = {"html:reports/cucumber.html"})
+public class TestNGTestRunner extends AbstractTestNGCucumberTests {
+}
---
+++ Selenium_TestNG_Cucumber_Template/src/test/java/template/stepDefinition/StepDefinitionImpl.java
@@ -0,0 +1,8 @@
+package template.stepDefinition;
+
+import template.testComponents.BaseTest;
+
+public class StepDefinitionImpl extends BaseTest {
+    /// step definition menthods of feature file.
+
+}
