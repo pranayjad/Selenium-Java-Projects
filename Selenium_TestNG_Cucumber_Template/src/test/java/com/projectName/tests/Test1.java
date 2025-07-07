19532edf99d720fb06fdd91fbaa3ee6c1ce1636d
pranayjad <pranayjad@gmail.com>
2025-07-07 16:51:08 +1200

    Selenium TestNG Cucumber Framework

--- Selenium_TestNG_Cucumber_Template/src/test/java/com/projectName/tests/Test1.java
+++ Selenium_TestNG_Cucumber_Template/src/test/java/com/projectName/tests/Test1.java
@@ -1,6 +1,5 @@
 package com.projectName.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import com.projectName.testComponents.BaseTest;
 import org.testng.annotations.Test;
@@ -9,7 +8,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
-public class Test1 extends BaseTest{
+public class Test1 extends BaseTest {
 
     @Test(groups={"Scenario"}, dataProvider = "getExcelData", priority = 1, description = "Test Case ID and Name")
     public void testCase()
